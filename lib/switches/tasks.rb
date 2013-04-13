e1c4d7f684ce451cad0a650eb6c7b4501d04defa
John Pignata <john@pignata.com>
2013-04-12 20:40:03 -0400

    Extract database connection to DRY up rake tasks

--- lib/switches/tasks.rb
+++ lib/switches/tasks.rb
@@ -4,30 +4,24 @@ require "switches/backends/postgres/tasks/remove"
 
 namespace :switches do
   namespace :postgres do
-    desc "Setup Switches PostgreSQL table"
-    task :setup do
+    task :database do
       database_url = ENV["DATABASE_URL"]
 
       unless database_url
         abort "DATABASE_URL required. (e.g. postgres://user:password@hostname:port/dbname)"
       end
 
-      connection = Switches::Backends::Postgres::Connection.new(database_url)
-      task = Switches::Backends::Postgres::Setup.new(connection)
-      task.run
+      @connection = Switches::Backends::Postgres::Connection.new(database_url)
+    end
+
+    desc "Setup Switches PostgreSQL table"
+    task setup: :database do
+      Switches::Backends::Postgres::Setup.new(@connection).run
     end
 
     desc "Remove Switches PostgreSQL table"
-    task :remove do
-      database_url = ENV["DATABASE_URL"]
-
-      unless database_url
-        abort "DATABASE_URL required. (e.g. postgres://user:password@hostname:port/dbname)"
-      end
-
-      connection = Switches::Backends::Postgres::Connection.new(database_url)
-      task = Switches::Backends::Postgres::Remove.new(connection)
-      task.run
+    task remove: :database do
+      Switches::Backends::Postgres::Remove.new(@connection).run
     end
   end
 end
